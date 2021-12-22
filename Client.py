cb512c58e8f1c4bf58f16af6ddc3970713c059d8
shaharkr <shahar.kramer@gmail.com>
2021-12-22 21:32:03 +0200

    Successfull Handshake

--- Client.py
+++ Client.py
@@ -1,4 +1,6 @@
 import socket
+import struct
+
 class Client:
     def __init__(self):
         self.udp_port = 13117
@@ -7,17 +9,52 @@ class Client:
         self.udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.udp_socket.bind(("",self.udp_port))
         #Message format
-        self.magicCookie = 0xabcddcba
+        self.udp_format = 'IbH'
+        self.magic_cookie = 0xabcddcba
         self.message_type = 0x2
+        self.tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.team_name = "Ben@Shahar"
+
+
     def find_server(self):
         print("shahar")
         while True:
             try:
                 data, address = self.udp_socket.recvfrom(1024)
+                message = struct.unpack(self.udp_format, data)
                 print(data)
                 print(address)
-            except:
+            except struct.error:
                 print("except")
+            if message[0] == self.magic_cookie and message[1] == self.message_type:
+                print(f"Received offer from {address[0]}, attempting to connect...")
+                break
+        self.tcp_connect_to_game(address[0], int(message[2]))
+
+    def tcp_connect_to_game(self, ip_server, port_server):
+        try:
+            self.conn_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.conn_tcp.connect((ip_server, port_server))
+        except Exception as e:
+            print(f" connection failed , reconnecting ...")
+            # if connection is failed changes the variable is_playing
+            self.is_palying = False
+            return
+        # send client name's team for the game
+        self.conn_tcp.send(self.team_name.encode('utf-8'))
+        # self.is_palying = True
+
+
+
+
+
+
+
+
+
+
+
+
 
 client = Client()
 client.find_server()
--- Server.py
+++ Server.py
@@ -1,34 +1,93 @@
 import socket
 from datetime import datetime
-#from scapy.arch import get_if_addr
+import threading
+from scapy.arch import get_if_addr
+import time
+from threading import * #Thread, acquire
+import struct
 
 class Server:
     link_proto = 'eth1'
+    buff_size = 1024
     def __init__(self, tcp_port):
         self.udp_port = 13117
         self.tcp_port = tcp_port
 
         #Message format
-        self.magicCookie = 0xabcddcba
-        self.message_type = 'Ben'
-
+        self.magic_cookie = 0xabcddcba
+        self.message_type = 0x2
+        self.udp_format = 'IbH'
         #Create a new UDP server socket
         #family - IVP4 addresses, type - UDP protocol
         self.broad_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
         self.broad_socket.setsockopt(socket.SOL_SOCKET,socket.SO_BROADCAST,1)
-        self.ip = socket.gethostbyname(socket.gethostname()) #get_if_addr(Server.link_proto) 
+        self.ip =  socket.gethostbyname(socket.gethostname()) #get_if_addr("eth1") 
+        self.keep_broadcasting = True
         #self.broad_socket.bind(('', self.udp_port))
-    def broadcast(self):
+        self.welcome_tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.welcome_tcp_socket.bind((self.ip, self.tcp_port))
+        self.lock_team_dict = threading.Lock()
+        self.teams_details = {}
+
+
+    def send_broadcast(self):
         now = datetime.now()
         print("Server started, listening on IP address " + self.ip)
+        message = struct.pack(self.udp_format, self.magic_cookie, self.message_type, self.tcp_port)
+        while self.keep_broadcasting:
+            self.broad_socket.sendto(message, ('<broadcast>', self.udp_port))
+            time.sleep(1)
+
+    def welcome_clients(self):
+        self.welcome_tcp_socket.settimeout(1000)
+        self.welcome_tcp_socket.listen()
+        lst = []
+        while len(self.teams_details.keys()) < 2:
+            try:
+                (client_socket,(client_ip, client_port)) = self.welcome_tcp_socket.accept()
+                t = Thread(target=self.handle_client, args=(client_socket,client_ip, client_port), daemon=True)
+                t.start()
+                lst.append(t)
+            except socket.timeout:
+                self.keep_broadcasting = False
+                break
+        for t in lst:
+            t.join()
+        
+        
+
+    def handle_client(self,client_socket,client_ip, client_port):
+        team_name = client_socket.recv(Server.buff_size).decode()
+        print(team_name)
+        self.lock_team_dict.acquire()
+        if len(self.teams_details.keys()) < 2:
+            self.teams_details[team_name] = [client_socket,client_ip, client_port]
+        self.lock_team_dict.release()
+    
+    def init_server(self):
         while True:
-            #self.broad_socket.sendto('')
-            self.broad_socket.sendto(self.message_type.encode(), ('<broadcast>', self.udp_port))
-            #message, clientAddress = self.broad_socket.recvfrom()
+            t1 = Thread(target=self.send_broadcast, daemon=True)
+            t2 = Thread(target=self.welcome_clients, daemon=True)
+
+            t1.start()
+            t2.start()
+
+            t1.join()
+            t2.join()
+
+            # self.game_mode()
+
+            # time.sleep(10)
+            # self.is_palying = False
+
+            # self.finish_game()
+
+
+
 
 
 server = Server(18121)
-server.broadcast()
+server.init_server()
         
 
 
