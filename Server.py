66766d32100a17ceec782bf30c9daf5067514e1f
BenAidlin <ben.aidlin@gmail.com>
2021-12-29 16:59:09 +0200

    git commit from remote(forgot to add)

--- Server.py
+++ Server.py
@@ -5,6 +5,7 @@ from scapy.arch import get_if_addr,get_if_list
 import time
 from threading import * #Thread, acquire
 import struct
+from Extensions import Colors, MathProblems
 
 class Server:
     link_proto = 'eth1'
@@ -43,7 +44,7 @@ class Server:
         self.tcp_port = self.welcome_tcp_socket.getsockname()[1]
 
     def send_broadcast(self):
-        print("Server started, listening on IP address " + self.ip)
+        print(Colors.OKBLUE + "Server started, listening on IP address " + self.ip)
         message = struct.pack(self.udp_format, self.magic_cookie, self.message_type, self.tcp_port)
         while self.keep_broadcasting:
             self.broad_socket.sendto(message, (self.subnet_broadcast_ip, self.udp_port))
@@ -73,7 +74,7 @@ class Server:
                         print(team_name + " was close")
                     self.game_started = False
                     self.teams_details = {}
-                print('Temporarily not acceptig clients')
+                print(Colors.WARNING + 'Temporarily not acceptig clients')
                 break
         self.keep_broadcasting = False
         print("------------")
@@ -85,7 +86,7 @@ class Server:
         if len(self.teams_details.keys()) < 2:
             self.teams_details[team_name] = [client_socket,client_ip, client_port]
         else:
-            sorry_string = 'Sorry '+team_name+', too many players'
+            sorry_string = Colors.WARNING + 'Sorry '+team_name+', too many players'
             client_socket.send(sorry_string.encode('utf-8'))
             client_socket.shutdown(socket.SHUT_RD)
             client_socket.close()
@@ -93,45 +94,50 @@ class Server:
     
     def start_game(self):
         if not self.game_started: # TODO: handle with 1 good connection
-            print("walla meztaer ach sheli")
+            print(Colors.WARNING + "Not enough players.. Cant start game. Disconnecting...")
             return
         lst = []
+        problem, solution = MathProblems.generate_problem()
         for team_name, details in self.teams_details.items():
-            t = Thread(target=self.send_question, daemon=True,args=[team_name,details])
+            t = Thread(target=self.send_question, daemon=True,args=[team_name,details,problem,solution])
             t.start()
             lst.append(t)
         for t in lst:
             t.join()
 
-    def send_question(self,team_name, details):
+    def send_question(self,team_name, details, problem, solution):
         details[0].settimeout(10)
         try:
-            welcome_string = "Welcome to Quick Maths.\nPlayer 1: " + list(self.teams_details.keys())[0] \
+            welcome_string = Colors.OKCYAN + "Welcome to Quick Maths.\nPlayer 1: " + list(self.teams_details.keys())[0] \
                 + "\nPlayer 2: " + list(self.teams_details.keys())[1] \
                     + "\n==\nPlease asnwer the following question as fast as you can:\n" \
-                        + "How mush is " + "2+2" + "?" 
+                        + "How mush is " + problem + "?" 
             details[0].send(welcome_string.encode('utf-8'))
             answer = details[0].recv(Server.buff_size).decode('utf-8')
             self.lock_answer.acquire()
             if(self.team_win is None):
                 # I answered first
-                if(str(answer) == "4"):
+                if(str(answer) == solution):
                     self.team_win = team_name
                 else:
                     self.team_win = [i for i in self.teams_details.keys() if i!=team_name][0]
-                over_string = "Game over!\nThe correct answer was " + "4" + "!\n\nCongratulations to the winner: " \
+                over_string = Colors.OKGREEN + "Game over!\nThe correct answer was " \
+                    + solution + "!\n\nCongratulations to the winner: " \
                     + str(self.team_win)
                 for team_name, conn in self.teams_details.items():
                     conn[0].send(over_string.encode('utf-8'))
             self.lock_answer.release()
         except socket.timeout:
-            print('timeout')
-        
-
+            print(Colors.WARNING+'timeout')
+        except: 
+            pass
     def finish_game(self):
         for team_name, conn in self.teams_details.items():
-            conn[0].shutdown(socket.SHUT_RD)
-            conn[0].close()
+            try:
+                conn[0].shutdown(socket.SHUT_RD)
+                conn[0].close()
+            except:
+                pass
         self.magic_cookie = 0xabcddcba
         self.message_type = 0x2
         self.udp_format = 'IbH'
